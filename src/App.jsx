a647b1ac7fd0036c914623ec2d1d38b5a3343cac
FrancoRosa <km115.franco@gmail.com>
2024-03-11 09:41:56 -0500

    Resize canvas and show bounding boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import { cloneElement, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { roboflow } from "./js/roboflow";
 import "./App.css";
+import { drawRect } from "./js/detection";
 
 const App = () => {
   const [range, setRange] = useState("all");
+  const [msg, setMsg] = useState(" - - - ");
   const [selectedFile, setSelectedFile] = useState(null);
+  const [width, setWidth] = useState(640);
+  const [height, setHeight] = useState(480);
+  const [model, setModel] = useState();
+  const [pause, setPause] = useState(false);
+  const [ranges, setRanges] = useState({
+    person: {
+      near: 25,
+      warning: 40,
+      danger: 45,
+    },
+    pile: {
+      near: 25,
+      warning: 40,
+      danger: 45,
+    },
+    crane: {
+      near: 25,
+      warning: 40,
+      danger: 45,
+    },
+  });
+  const videoRef = useRef();
+  const cnvRef = useRef();
+  const cnvRef2 = useRef();
+  let interval;
 
   const handleRange = (e) => {
     setRange(e.target.value);
+    console.log(e.target.value);
   };
 
   const handleFile = (e) => {
@@ -19,14 +48,84 @@ const App = () => {
     setSelectedFile();
   };
 
+  const handlePause = () => {
+    if (!pause) {
+      clearInterval(interval);
+    }
+    setPause((s) => !s);
+  };
+
+  const handleMetadata = () => {
+    const wd = videoRef.current.videoWidth;
+    const ht = videoRef.current.videoHeight;
+    cnvRef.current.height = ht;
+    cnvRef.current.width = wd;
+    cnvRef2.current.height = ht;
+    cnvRef2.current.width = wd;
+    setWidth(wd);
+    setHeight(ht);
+  };
+
+  useEffect(() => {
+    setMsg("... loading model");
+
+    try {
+      // eslint-disable-next-line no-undef
+      roboflow
+        .auth({
+          publishable_key: "rf_5w20VzQObTXjJhTjq6kad9ubrm33",
+        })
+        .load({
+          // model: "trees-88tmr",
+          model: "rocks-kmf7f",
+          version: 2,
+        })
+        .then(function (mdl) {
+          setMsg("... model ready");
+          setModel(mdl);
+        })
+        .catch((e) => {
+          setMsg("... error while loading model");
+          console.log(e);
+        });
+    } catch (error) {
+      setMsg(`... ${error.message}`);
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (model && !pause) {
+      interval = setInterval(() => {
+        try {
+          model.detect(videoRef.current).then(function (predictions) {
+            const ctx = cnvRef.current.getContext("2d");
+            drawRect(predictions, ctx, width, height, range, ranges, cnvRef2);
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [model, range, pause]);
+
   return (
     <>
       {selectedFile && (
         <div style={{ position: "relative" }} id="layer">
-          <canvas id="canvas1"></canvas>
-          <canvas id="canvas2"></canvas>
+          <canvas ref={cnvRef}></canvas>
+          <canvas ref={cnvRef2}></canvas>
 
-          <video autoPlay muted controls>
+          <video
+            autoPlay
+            muted
+            controls
+            ref={videoRef}
+            onLoadedMetadata={handleMetadata}
+          >
             <source
               src={URL.createObjectURL(selectedFile)}
               type={selectedFile.type}
@@ -41,6 +140,8 @@ const App = () => {
       {!selectedFile && (
         <>
           <h1>Object detector validator</h1>
+          <p id="message">{msg}</p>
+
           <div style={{ display: "flex", justifyContent: "center" }}>
             <input
               id="file"
@@ -62,13 +163,13 @@ const App = () => {
             gap: "1em",
           }}
         >
-          <p id="message">---</p>
           <select id="distance" onChange={handleRange}>
             <option value="all" defaultValue>
               All
             </option>
             <option value="near">Near</option>
           </select>
+          <button onClick={handlePause}>{pause ? "Resume" : "Pause"}</button>
           <button onClick={handleRemove}>Change file</button>
         </div>
       )}
